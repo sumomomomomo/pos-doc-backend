4dc6e50f1be412539205d0d269c8ff00818de830
sumomomomomo <matthew.hoph@outlook.sg>
2026-08-29 12:04:21 +0800

    Add spring boot quickstart

---
+++ pos-doc-backend/src/main/java/horse/sumomo/pos_doc_backend/PosDocBackendApplication.java
@@ -0,0 +1,13 @@
+package horse.sumomo.pos_doc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosDocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosDocBackendApplication.class, args);
+	}
+
+}
---
+++ pos-doc-backend/src/test/java/horse/sumomo/pos_doc_backend/PosDocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package horse.sumomo.pos_doc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosDocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
